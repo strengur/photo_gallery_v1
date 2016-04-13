fe676829b818409a95f1872fac4868b110122c90
Steingrímur <steingrimur@Steingrimurs-Mac-mini.local>
2016-04-13 18:32:36 +0000

    Minor changes

--- js/app.js
+++ js/app.js
@@ -2,8 +2,8 @@ $("img").click(function() {
   $('div[id="overlay"]').addClass("darkTransparent");
   var $altText = $(this).attr("alt");
   var $innerHTML = "";
-  $innerHTML += '<h4 id="close-slideshow">+</h4>';
-  $innerHTML += '<img src="assets/Photos/01.jpg">';
+  $innerHTML += '<div><h4 id="close-slideshow">+</h4>';
+  $innerHTML += '<img src="assets/Photos/01.jpg"></div>';
   $innerHTML += '<p>';
   $innerHTML += $altText;
   $innerHTML += '</p>';
