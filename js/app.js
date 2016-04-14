8e6686cce68d23857d69321b45356375cdf85781
Steingrímur <steingrimur@Steingrimurs-Mac-mini.local>
2016-04-14 08:49:57 +0000

    Lightbox work

--- js/app.js
+++ js/app.js
@@ -2,15 +2,17 @@ $("img").click(function() {
   $('div[id="overlay"]').addClass("darkTransparent");
   var $altText = $(this).attr("alt");
   var $innerHTML = "";
-  $innerHTML += '<div><h4 id="close-slideshow">+</h4>';
+  $innerHTML += '<div id="large-slide-image"><h4 id="close-slideshow">+</h4>';
   $innerHTML += '<img src="assets/Photos/01.jpg"></div>';
   $innerHTML += '<p>';
   $innerHTML += $altText;
   $innerHTML += '</p>';
   $(".image-slide").append($innerHTML);
 
+});
 
+$(".image-slide").click(function() {
+  $("#large-slide-image").remove();
+  $("#overlay").removeClass("darkTransparent");
 
-
-  //$("p").text( $altText );
 });
