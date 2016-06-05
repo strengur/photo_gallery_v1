195ecae79fd4f75e4152837fe3957577c63f806c
Steingrímur <steingrimur@740.is>
2016-06-05 22:29:49 +0000

    Revert "Lots of things"
    This reverts commit 5cf6d4cda9051ae4c799e2ca30cb835e515de74b.
    
    # Conflicts:
    #	index.html
    #	js/app.js

--- js/app.js
+++ js/app.js
@@ -1,15 +1,7 @@
+
 $("img").click(function() {
   $('div[id="overlay"]').addClass("darkTransparent");
-  var $altText = $(this).attr("alt");
-  var $innerHTML = "";
-  $innerHTML += '<div id="large-slide-image"><h4 id="close-slideshow">+</h4>';
-  $innerHTML += '<img src="assets/Photos/01.jpg"></div>';
-  $innerHTML += '<p>';
-  $innerHTML += $altText;
-  $innerHTML += '</p>';
-  $(".image-slide").append($innerHTML);
 
-<<<<<<< HEAD
   var altText = "";
   var srcUrl = "";
   var fileName ="";
@@ -19,24 +11,28 @@ $("img").click(function() {
   srcUrl = $(this).attr("src");
   fileName = srcUrl.slice(srcUrl.lastIndexOf('/'));
 
-  // innerHTML += '<div id="large-slide-image">';
-  // innerHTML += '<div class="previous"><img src="assets/icon/left-arrow.svg"></div>';
-  // innerHTML += '<div class="next"><img src="assets/icon/right-arrow.svg"></div>';
+  innerHTML += '<div id="large-slide-image">';
+  innerHTML += '<div class="previous"><img src="assets/icon/left-arrow.svg"></div>';
+  innerHTML += '<div class="next"><img src="assets/icon/right-arrow.svg"></div>';
   innerHTML += '<img src="assets/Photos/';
   innerHTML += fileName;
   innerHTML += '">';
   innerHTML += '<p>';
   innerHTML += altText;
   innerHTML += '</p>';
-  // innerHTML += '<div class="close"><img id="close-slideshow" src="assets/icon/close.svg"></div>';
-  // innerHTML += '</div>';
+  innerHTML += '<div class="close"><img id="close-slideshow" src="assets/icon/close.svg"></div>';
+  innerHTML += '</div>';
   $(".image-slide").append(innerHTML);
-=======
->>>>>>> parent of 5cf6d4c... Lots of things
 });
 
-$(".image-slide").click(function() {
+$('.close').click(function() {
+  console.log('Clicked');
+  $(".image-slide p").remove();
   $("#large-slide-image").remove();
   $("#overlay").removeClass("darkTransparent");
 
+});
+
+$('.next').click(function()) {
+  console.log('Next has been clicked!');
 });
