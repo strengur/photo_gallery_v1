c312d6a7701c5f11c3b7466474a592679fdd10a7
Steingrímur <steingrimur@740.is>
2016-06-09 19:43:07 +0000

    Light box up
    It is now displaying the clicked thumbnail image on a black overlay.

--- js/app.js
+++ js/app.js
@@ -1,38 +1,64 @@
+// Set the gloabal varable for large image path.
+$folderPath = "assets/Photos/";
 
-$("img").click(function() {
-  $('div[id="overlay"]').addClass("darkTransparent");
+// Set variable for the elements that will be added to the body when thumbnail is clicked.
 
-  var altText = "";
-  var srcUrl = "";
-  var fileName ="";
-  var innerHTML = "";
+var $overlay = $('<div id="overlay"></div>');
+var $image = $("<img>");
+var $caption = $("<p></p>");
 
-  altText = $(this).attr("alt");
-  srcUrl = $(this).attr("src");
-  fileName = srcUrl.slice(srcUrl.lastIndexOf('/'));
+// Append elements to overlay's div
+$overlay.append($image);
+$overlay.append($caption);
 
-  innerHTML += '<div id="large-slide-image">';
-  innerHTML += '<div class="previous"><img src="assets/icon/left-arrow.svg"></div>';
-  innerHTML += '<div class="next"><img src="assets/icon/right-arrow.svg"></div>';
-  innerHTML += '<img src="assets/Photos/';
-  innerHTML += fileName;
-  innerHTML += '">';
-  innerHTML += '<p>';
-  innerHTML += altText;
-  innerHTML += '</p>';
-  innerHTML += '<div class="close"><img id="close-slideshow" src="assets/icon/close.svg"></div>';
-  innerHTML += '</div>';
-  $(".image-slide").append(innerHTML);
+// Append the complete overlay to the body
+$("body").append($overlay);
+
+// When a thumbnail is clicked the the overlay is showed.
+$("#imageGalleryList a").click(function(event) {
+  event.preventDefault();
+
+  // Get and set basic variables to be used to display correct image and caption.
+  var $imagePath = $(this).attr("href");
+    // Use the slice to get only the file name of the thumbnail.
+  var $fileName = $imagePath.slice($imagePath.lastIndexOf('/'));
+    // Combine the path to large images and the file name to be used later on when adding the image source.
+  var $fullPathAndName = $folderPath.concat($fileName);
+    // Get the alt text from the image of selected element by using .childre() to get to the img element.
+  var $imageCaption = $(this).children("img").attr("alt");
+
+  $image.attr("src", $fullPathAndName);
+  $caption.text($imageCaption);
+
+  $overlay.show();
+
+  // Get href path for the previous image to work with and add it to global varable.
+  $previous = $(this).parent().prev("li").children().attr("href");
+  if ($previous != undefined) {
+    console.log("Í lagi");
+  } else {
+    console.log("Ekki í lagi");
+    // Fade out right arrow and disable it
+  }
+
+  // Get href path for the next image to work with and add it to global varable.
+  $next = $(this).parent().next("li").children().attr("href");
+  if ($next != undefined) {
+    console.log("Í lagi");
+  } else {
+    console.log("Ekki í lagi");
+    // Fade out right arrow and disable it
+  }
+
+
 });
 
-$('.close').click(function() {
-  console.log('Clicked');
-  $(".image-slide p").remove();
-  $("#large-slide-image").remove();
-  $("#overlay").removeClass("darkTransparent");
-
+$("#overlay").click(function() {
+  $overlay.hide();
+  console.log($previous);
+  console.log($next);
 });
 
-$('.next').click(function()) {
+$('.next').click(function() {
   console.log('Next has been clicked!');
 });
